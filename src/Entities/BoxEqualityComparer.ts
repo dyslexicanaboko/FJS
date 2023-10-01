3f91173f94df01588944c27c137128ee1e0eed6a
Eli Hayon <dyslexicanaboko@gmail.com>
2023-10-01 00:21:40 -0400

    #12 Created all the plumbing required to exercise the IEqualityComparer and re-ran all of the dictionary tests, but with the Equality Comparer instead of the object's default compare.

---
+++ src/Entities/BoxEqualityComparer.ts
@@ -0,0 +1,33 @@
+//I am shamelessly copying the example from the documentation
+//https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.iequalitycomparer-1
+//https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.equalitycomparer-1
+
+import IEqualityComparer from "../System/Collections/Generic/IEqualityComparer.js";
+import { getHashCodeForNumber } from "../utils.js";
+import BoxObject from "./BoxObject.js";
+
+export default class BoxEqualityComparer
+  implements IEqualityComparer<BoxObject>
+{
+  public equals(
+    left: BoxObject | undefined,
+    right: BoxObject | undefined
+  ): boolean {
+    if (left === undefined && right === undefined) return true;
+
+    if (left === undefined || right === undefined) return false;
+
+    return (
+      left.height === right.height &&
+      left.length === right.length &&
+      left.width === right.width
+    );
+  }
+
+  public getHashCode(obj: BoxObject): number {
+    const hCode = obj.height ^ obj.length ^ obj.width;
+
+    //This function call can be skipped, but it is here for demonstration purposes.
+    return getHashCodeForNumber(hCode);
+  }
+}
--- src/System/Collections/Generic/IEqualityComparer.ts
+++ src/System/Collections/Generic/IEqualityComparer.ts
@@ -3,3 +3,18 @@ export default interface IEqualityComparer<T> {
   equals(x: T | undefined, y: T | undefined): boolean;
   getHashCode(obj: T): number;
 }
+
+/*
+https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.equalitycomparer-1
+
+Normally I would follow this advice:
+-----
+We recommend that you derive from the EqualityComparer<T> class instead of implementing the
+IEqualityComparer<T> interface, because the EqualityComparer<T> class tests for equality using 
+the IEquatable<T>.Equals method instead of the Object.Equals method. This is consistent with 
+the Contains, IndexOf, LastIndexOf, and Remove methods of the Dictionary<TKey,TValue> class 
+and other generic collections.
+-----
+
+But in this case using an interface will be fine since this is all a foreign concept anyhow.
+*/
